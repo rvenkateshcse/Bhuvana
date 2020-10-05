ca2dcf6daf4ff5f0cc92dfbf89a30ecf64b0507d
Venkatesh <rvenkateshcse@outlook.com>
2020-10-05 15:03:51 +0530

    HomePage

--- StudentAttendenceManagementProject/StudentAttendenceManagementProject/Controllers/HomeController.cs
+++ StudentAttendenceManagementProject/StudentAttendenceManagementProject/Controllers/HomeController.cs
@@ -10,11 +10,12 @@ namespace StudentAttendenceManagementProject.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(string Un,string Pwd,string Rol)
+        public ActionResult Index(string Un,string Pwd,string Rol,string Receiver,string subject,string Message)
         {
             int re = Services.LogInservice.Login(Un, Pwd);
             if (re == 1)
             {
+
                 if (Rol == "staff")
                 {
                     return RedirectToAction("Staff", "StaffPage");
