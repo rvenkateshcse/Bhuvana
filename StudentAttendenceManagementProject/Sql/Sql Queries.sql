835f7a8eed3fcd397989a5924e46d01841032793
Venkatesh <rvenkateshcse@outlook.com>
2020-11-27 19:28:15 +0530

    Sql Aggregate Functions

--- StudentAttendenceManagementProject/Sql/Sql Queries.sql
+++ StudentAttendenceManagementProject/Sql/Sql Queries.sql
@@ -4,8 +4,11 @@ create table Book(Id int,Name varchar(50),Cost Money)
 /*Sql Query to insert values to table*/
 insert into Book values(1,'Tamil',180)
 insert into Book values(2,'English',160)
+insert into Book values(3,'Maths',190)
+insert into Book values(4,'Science',200)
+insert into Book values(5,'Social Science',210)
 
-/*Sql Query to select all columns table*/
+/*Sql Query to select all columns from table*/
 select*from Book;
 
 /*Sql Query to Delete table*/
@@ -16,6 +19,23 @@ select Name,Cost from Book;
 
 /*Sql Query to select specific rows from table which satisfies condition*/
 select * from Book where id>1
+
+/*Sql Query to return average of column*/
+select AVG(cost) as AveragePrice from Book;
+
+/*Sql Query to select Minimum value of Expression*/
+select  min(cost) LowCostPrice from Book;
+
+/*Sql Query to select Maximum value of Expression*/
+select  max(cost) HighCostPrice from Book;
+
+/*Sql Query to find sum of values*/
+select sum(cost) Total from Book;
+
+/*Sql Query to return number of rows*/
+select count(Name) "No.of.books" from Book
+
+
 
 
 
