7e8f0aeda8bef89b8a581ca339a6d6cca49d3239
Venkatesh <rvenkateshcse@outlook.com>
2020-12-09 10:57:43 +0530

    sql query to dd-mm-yyyy to yyyy-mm-dd and Largest of three numbers

---
+++ StudentAttendenceManagementProject/Sql/Sql Query.sql
@@ -0,0 +1,19 @@
+/*Sql Query to get today date*/
+select GETDATE() as TodayDate
+
+/*Sql Query to convert dd-mm-yyyy to yyyy-mm-dd*/
+declare  @Date varchar(10)='09-12-2020'
+select CONVERT(date,@Date,103)
+
+/*Sql Query to find largest of three numbers */
+declare @a int =10;
+declare @b int =30;
+declare @c int =40;
+if(@a>@b)and (@a>@c)
+print'a is greater'
+else if(@b>@a)and (@b>@c)
+print'b is greater'
+else
+print 'c is greater'
+
+
