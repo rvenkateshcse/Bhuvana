d1da2ea92b268e4158426a37f915dfdd82534f08
Venkatesh <rvenkateshcse@outlook.com>
2021-01-11 21:13:07 +0530

    sync in

---
+++ StudentAttendenceManagementProject/Convert String To Lowercase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Convert_String_To_Lowercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String");
+            string str = Console.ReadLine();
+            foreach(char ch in str)
+            {
+                if(ch>=65&&ch<=90)
+                {
+                    char d = (char)(ch + 32);
+                    Console.Write(d);
+                }
+                else
+                {
+                    char z = ch;
+                    Console.Write(z);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StudentAttendenceManagementProject/Power of four/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Power_of_four
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter num");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            int res = 1;
+            while(res<Num)
+            {
+                res *= 4;
+            }
+            if (res == Num)
+            
+                Console.WriteLine("yes");
+            
+            else
+                Console.WriteLine("no");
+            Console.ReadLine();
+        }
+    }
+}
